b6b9b738914fc88f596b7df3f30627e6af1203a1
Dheerajydv <dy81035@gmail.com>
2024-08-10 15:52:46 +0530

    Basic routes and setup done (Frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../src/pages/Login";
+import Navbar from "../src/components/Navbar";
+
 function App() {
   return (
     <>
-      <div className="bg-red-500 text ">Hello World</div>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/components/EntryBox.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EntryBox() {
+  return <div>EntryBox</div>;
+}
