f023e847ec9d1b945027961f42e825f7d3d1307c
Dheeraj Yadav <dy81035@gmail.com>
2025-04-05 09:51:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:4000",
+      "/api": "https://to-do-app-pdu9.onrender.com",
     },
   },
 });
